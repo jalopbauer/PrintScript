88a014a1a59ff11be01a8f989914a4443fcc0492
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-03-12 14:12:20 -0300

    LineLexer

--- src/main/kotlin/lexer/Lexer.kt
+++ src/main/kotlin/lexer/Lexer.kt
@@ -4,6 +4,13 @@ import token.Token
 
 interface Lexer<T :LexerInput> {
 
-    fun buildTokenList(lexerInput: LexerInput): List<Token>
+    fun buildTokenList(lexerInput: T): List<Token>
+
+}
+
+class LineLexer: Lexer<Line> {
+    override fun buildTokenList(line: Line): List<Token> {
+        TODO("Poner aca lo de partir un linea")
+    }
 
 }
