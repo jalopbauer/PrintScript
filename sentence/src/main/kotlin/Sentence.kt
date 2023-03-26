efae7356c979021d6b7d86f5d7260b0ab4f3cbb0
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-03-26 11:26:36 -0300

    SentenceBuilder

--- sentence/src/main/kotlin/Sentence.kt
+++ sentence/src/main/kotlin/Sentence.kt
@@ -1,3 +1,8 @@
-class Sentence {
+import token.Token
 
-}
+sealed interface Sentence
+data class Declaration(val variableName: Token, val variableType: Token): Sentence
+data class Println(val tokensInsideParenthesis: List<Token>): Sentence
+data class Assignation(val tokensInsideParenthesis: List<Token>): Sentence
+data class DeclarationAssignation(val declaration: Declaration, val assignation: Assignation): Sentence
+
