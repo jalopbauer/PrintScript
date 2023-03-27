06fb16e706ff24ccd11ba970015b48b4760f3a9d
Rodri Marchese <rodrimarchese@gmail.com>
2023-03-27 14:01:59 -0300

    pre-commit script fix. changed order. first formater and then checks

--- sentence/src/main/kotlin/Sentence.kt
+++ sentence/src/main/kotlin/Sentence.kt
@@ -2,6 +2,6 @@ import token.Token
 
 sealed interface Sentence
 data class Declaration(val variableName: Token, val variableType: Token) : Sentence
-data class Println(val tokensInsideParenthesis: List<Token>):Sentence
+data class Println(val tokensInsideParenthesis: List<Token>) : Sentence
 data class Assignation(val tokensInsideParenthesis: List<Token>) : Sentence
 data class DeclarationAssignation(val declaration: Declaration, val assignation: Assignation) : Sentence
