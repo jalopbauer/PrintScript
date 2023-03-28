ad533a2d6db4f5926afb71fdfd6a07b8ac86227f
Rodri Marchese <rodrimarchese@gmail.com>
2023-03-28 18:02:17 -0300

    testing2

--- parser/src/main/kotlin/Parser.kt
+++ parser/src/main/kotlin/Parser.kt
@@ -5,12 +5,11 @@ interface Parser {
     fun parse(nextToken: Token, previousRelevantTokens: List<Token>): ParserResponse
 }
 
-class ParserImpl(private val sentenceBuilder: SentenceBuilder):Parser {
+class ParserImpl(private val sentenceBuilder: SentenceBuilder) : Parser {
     override fun parse(nextToken: Token, previousRelevantTokens: List<Token>): ParserResponse {
         val tokens = previousRelevantTokens + nextToken
-        return if (nextToken.tokenName == TokenName.SEMICOLON)   {
+        return if (nextToken.tokenName == TokenName.SEMICOLON) {
             val sentence = sentenceBuilder.build(tokens)
-
 
             if (sentence != null) {
                 ValidSentence(sentence)
