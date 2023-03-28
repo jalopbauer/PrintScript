b29031afa6e337f206ba9ce1bacef0e7e9acd488
Rodri Marchese <rodrimarchese@gmail.com>
2023-03-28 17:58:09 -0300

    testing linter

--- parser/src/main/kotlin/Parser.kt
+++ parser/src/main/kotlin/Parser.kt
@@ -5,11 +5,13 @@ interface Parser {
     fun parse(nextToken: Token, previousRelevantTokens: List<Token>): ParserResponse
 }
 
-class ParserImpl(private val sentenceBuilder: SentenceBuilder) : Parser {
+class ParserImpl(private val sentenceBuilder: SentenceBuilder):Parser {
     override fun parse(nextToken: Token, previousRelevantTokens: List<Token>): ParserResponse {
         val tokens = previousRelevantTokens + nextToken
-        return if (nextToken.tokenName == TokenName.SEMICOLON) {
+        return if (nextToken.tokenName == TokenName.SEMICOLON)   {
             val sentence = sentenceBuilder.build(tokens)
+
+
             if (sentence != null) {
                 ValidSentence(sentence)
             } else {
