3e201203d9f7057069cb06ae98da5fb73bcf346c
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-04-04 16:25:49 -0300

    SentenceValidator

--- sentence/src/main/kotlin/Sentence.kt
+++ sentence/src/main/kotlin/Sentence.kt
@@ -1,31 +1,45 @@
 import token.Token
+import token.TokenName
+import token.VariableLiteralToken
 
 sealed interface Sentence {
     fun getAst(): AbstractSyntaxTree
 }
-data class Declaration(val variableName: Token, val variableType: Token) : Sentence {
+data class Declaration(val variableName: VariableLiteralToken, val variableType: Token) : Sentence {
     override fun getAst(): AbstractSyntaxTree {
-        return Node(
-            ":",
-            NodeValue(variableName.tokenName().name),
-            NodeValue(variableType.tokenName().name)
+        return ValuedNode(
+            NodeValue(TokenName.DECLARATION),
+            NodeValue(variableName.value),
+            NodeValue(variableType.tokenName())
         )
     }
 }
-
-data class Println(val tokensInsideParenthesis: List<Token>) : Sentence {
+data class Println(val stringValue: StringValue) : Sentence {
     override fun getAst(): AbstractSyntaxTree {
         TODO("Not yet implemented")
     }
 }
 
-data class Assignation(val tokensInsideParenthesis: List<Token>) : Sentence {
+data class Assignation(val value: Value) : Sentence {
     override fun getAst(): AbstractSyntaxTree {
         TODO("Not yet implemented")
     }
 }
 
 data class DeclarationAssignation(val declaration: Declaration, val assignation: Assignation) : Sentence {
+    override fun getAst(): AbstractSyntaxTree {
+        TODO("Not yet implemented")
+    }
+}
+
+interface Value : Sentence
+data class StringValue(val tokensInsideParenthesis: List<Token>) : Value {
+    override fun getAst(): AbstractSyntaxTree {
+        TODO("Not yet implemented")
+    }
+}
+
+data class NumberValue(val tokensInsideParenthesis: List<Token>) : Value {
     override fun getAst(): AbstractSyntaxTree {
         TODO("Not yet implemented")
     }
--- sentence/src/main/kotlin/SentenceValidator.kt
+++ sentence/src/main/kotlin/SentenceValidator.kt
@@ -4,22 +4,6 @@ import token.VariableLiteralToken
 
 interface SentenceValidator {
     fun build(tokenList: List<Token>): Sentence?
-    fun typeCheckString(tokens: List<Token>): Boolean =
-        !tokens
-            .any {
-                it.tokenName() != TokenName.SUM ||
-                    it.tokenName() != TokenName.STRING_LITERAL
-            }
-
-    fun typeCheckNumber(tokens: List<Token>): Boolean =
-        !tokens
-            .any {
-                it.tokenName() != TokenName.SUM ||
-                    it.tokenName() != TokenName.SUB ||
-                    it.tokenName() != TokenName.MULT ||
-                    it.tokenName() != TokenName.DIV ||
-                    it.tokenName() != TokenName.NUMBER_LITERAL
-            }
 }
 class PrintScriptValidator : SentenceValidator {
     override fun build(tokenList: List<Token>): Sentence? {
@@ -77,7 +61,8 @@ class PrintLnValidator : SentenceValidator {
         } else if (tokenList[tokenList.size - 2].tokenName() != TokenName.RIGHT_PARENTHESIS) {
             null
         } else {
-            Println(tokenList.subList(2, tokenList.size - 2))
+            StringSentenceValidator().build(tokenList.subList(2, tokenList.size - 2))
+                ?.let { Println(it) }
         }
     }
 }
@@ -90,10 +75,12 @@ class AssignationValidator : SentenceValidator {
             null
         } else if (tokenList.component2().tokenName() != TokenName.ASSIGNATION) {
             null
-        } else if (!(typeCheckString(tokenList.subList(2, tokenList.size-1))||typeCheckNumber(tokenList.subList(2, tokenList.size-1)))) {
-            null
         } else {
-            Assignation(tokenList.subList(2, tokenList.size - 2))
+            val tokensAfterEquals = tokenList.subList(2, tokenList.size - 2)
+            StringSentenceValidator().build(tokensAfterEquals)
+                ?.let { Assignation(it) }
+                ?: NumberSentenceValidator().build(tokensAfterEquals)
+                    ?.let { Assignation(it) }
         }
     }
 }
@@ -113,5 +100,38 @@ class DeclarationAssignationValidator : SentenceValidator {
     }
     private fun tokenListWithoutLetVariableType(tokenList: List<Token>): List<Token> {
         return tokenList.drop(0).drop(2).drop(3)
+    }
+}
+
+class StringSentenceValidator : SentenceValidator {
+    override fun build(tokenList: List<Token>): StringValue? {
+        return if (!tokenList
+            .any {
+                it.tokenName() != TokenName.SUM ||
+                    it.tokenName() != TokenName.STRING_LITERAL
+            }
+        ) {
+            StringValue(tokenList)
+        } else {
+            null
+        }
+    }
+}
+
+class NumberSentenceValidator : SentenceValidator {
+    override fun build(tokenList: List<Token>): NumberValue? {
+        return if (!tokenList
+            .any {
+                it.tokenName() != TokenName.SUM ||
+                    it.tokenName() != TokenName.SUB ||
+                    it.tokenName() != TokenName.MULT ||
+                    it.tokenName() != TokenName.DIV ||
+                    it.tokenName() != TokenName.NUMBER_LITERAL
+            }
+        ) {
+            NumberValue(tokenList)
+        } else {
+            null
+        }
     }
 }
