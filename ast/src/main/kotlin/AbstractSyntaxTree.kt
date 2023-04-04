5ee2f18f403221e08b345a9b8f00b5b88afd4874
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-04-04 17:37:46 -0300

    Ast generics

--- ast/src/main/kotlin/AbstractSyntaxTree.kt
+++ ast/src/main/kotlin/AbstractSyntaxTree.kt
@@ -1,4 +1,17 @@
-sealed interface AbstractSyntaxTree
-data class ValuedNode<T>(val value: NodeValue<T>, val lNode: AbstractSyntaxTree, val rNode: AbstractSyntaxTree) : AbstractSyntaxTree
-data class ValuedSingleNode<T>(val value: NodeValue<T>, val node: AbstractSyntaxTree) : AbstractSyntaxTree
-data class NodeValue<T>(val value: T) : AbstractSyntaxTree
+sealed interface AbstractSyntaxTree<T> {
+    fun value(): T
+}
+data class ValuedNode<T>(val value: T, val lNode: AbstractSyntaxTree<*>, val rNode: AbstractSyntaxTree<*>) : AbstractSyntaxTree<T> {
+    override fun value(): T =
+        value
+}
+
+data class ValuedSingleNode<T>(val value: T, val node: AbstractSyntaxTree<*>) : AbstractSyntaxTree<T> {
+    override fun value(): T =
+        value
+}
+
+data class NodeValue<T>(val value: T) : AbstractSyntaxTree<T> {
+    override fun value(): T =
+        value
+}
