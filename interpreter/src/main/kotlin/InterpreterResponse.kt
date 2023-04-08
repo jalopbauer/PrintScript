e0db46c2e01023b11fe2c2574a1bfd2054ead4ce
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-04-08 17:20:50 -0300

    Removed unused

--- interpreter/src/main/kotlin/InterpreterResponse.kt
+++ interpreter/src/main/kotlin/InterpreterResponse.kt
@@ -4,8 +4,6 @@ interface Error : InterpreterResponse {
 }
 
 interface InterpreterState : InterpreterResponse {
-    fun add(value: String): InterpreterState
     fun addVariableValueToPrintln(value: String): InterpreterResponse
     fun addValueToPrintln(value: String): InterpreterResponse
-    fun addValueToPrintln(value: Int): InterpreterResponse
 }
