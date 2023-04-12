be47a580e365892f323fd7809beef1f1e1095fcb
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-04-12 02:40:25 -0300

    StatefullPrintScriptInterpreterState.initializeVariable

--- interpreter/src/main/kotlin/InterpreterResponse.kt
+++ interpreter/src/main/kotlin/InterpreterResponse.kt
@@ -2,6 +2,10 @@
 interface Error {
     fun message(): String
 }
+class VariableAlreadyExistsError : Error {
+    override fun message(): String =
+        "VariableAlreadyExistsError"
+}
 
 class AstStructureNotDefinedError : Error {
     override fun message(): String =
@@ -10,7 +14,7 @@ class AstStructureNotDefinedError : Error {
 
 interface PrintScriptInterpreterState {
     fun addError(error: Error): PrintScriptInterpreterState
-    fun initializeVariable(value: VariableNameNode, type: TypeNode): PrintScriptInterpreterState
+    fun initializeVariable(key: VariableNameNode, value: TypeNode): PrintScriptInterpreterState
 
     fun println(value: VariableNameNode): PrintScriptInterpreterState
     fun println(value: NumberLiteralStringNode): PrintScriptInterpreterState
@@ -21,14 +25,16 @@ interface PrintScriptInterpreterState {
     fun setValueToVariable(variableNameNode: VariableNameNode, value: VariableNameNode): PrintScriptInterpreterState
 }
 
-data class StatefullPrintScriptInterpreterState(val errors: List<Error>) : PrintScriptInterpreterState {
+data class StatefullPrintScriptInterpreterState(val errors: List<Error>, val variableTypeMap: Map<String, String>) : PrintScriptInterpreterState {
     override fun addError(error: Error): PrintScriptInterpreterState =
         this.copy(errors = errors + error)
 
-    override fun initializeVariable(value: VariableNameNode, type: TypeNode): PrintScriptInterpreterState {
-        TODO("Not yet implemented")
-    }
-
+    override fun initializeVariable(key: VariableNameNode, value: TypeNode): PrintScriptInterpreterState =
+        if (isVariableDefined(key)) {
+            this.addError(VariableAlreadyExistsError())
+        } else {
+            this.copy(variableTypeMap = variableTypeMap + (key.value() to value.value()))
+        }
     override fun println(value: VariableNameNode): PrintScriptInterpreterState {
         TODO("Not yet implemented")
     }
@@ -61,4 +67,6 @@ data class StatefullPrintScriptInterpreterState(val errors: List<Error>) : Print
     ): PrintScriptInterpreterState {
         TODO("Not yet implemented")
     }
+
+    private fun isVariableDefined(key: VariableNameNode) = variableTypeMap.containsKey(key.value())
 }
