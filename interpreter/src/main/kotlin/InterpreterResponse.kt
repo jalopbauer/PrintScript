73b98d4587008dc8e6a7fcf662889842d4a7c854
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-04-12 02:27:35 -0300

    StatefullPrintScriptInterpreterState.addError

--- interpreter/src/main/kotlin/InterpreterResponse.kt
+++ interpreter/src/main/kotlin/InterpreterResponse.kt
@@ -9,7 +9,7 @@ class AstStructureNotDefinedError : Error {
 }
 
 interface PrintScriptInterpreterState {
-    fun addError(astStructureNotDefinedError: Error): PrintScriptInterpreterState
+    fun addError(error: Error): PrintScriptInterpreterState
     fun initializeVariable(value: VariableNameNode, type: TypeNode): PrintScriptInterpreterState
 
     fun println(value: VariableNameNode): PrintScriptInterpreterState
@@ -19,4 +19,46 @@ interface PrintScriptInterpreterState {
     fun setValueToVariable(variableNameNode: VariableNameNode, value: NumberNode): PrintScriptInterpreterState
     fun setValueToVariable(variableNameNode: VariableNameNode, value: StringNode): PrintScriptInterpreterState
     fun setValueToVariable(variableNameNode: VariableNameNode, value: VariableNameNode): PrintScriptInterpreterState
+}
+
+data class StatefullPrintScriptInterpreterState(val errors: List<Error>) : PrintScriptInterpreterState {
+    override fun addError(error: Error): PrintScriptInterpreterState =
+        this.copy(errors = errors + error)
+
+    override fun initializeVariable(value: VariableNameNode, type: TypeNode): PrintScriptInterpreterState {
+        TODO("Not yet implemented")
+    }
+
+    override fun println(value: VariableNameNode): PrintScriptInterpreterState {
+        TODO("Not yet implemented")
+    }
+
+    override fun println(value: NumberLiteralStringNode): PrintScriptInterpreterState {
+        TODO("Not yet implemented")
+    }
+
+    override fun println(value: StringNode): PrintScriptInterpreterState {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValueToVariable(
+        variableNameNode: VariableNameNode,
+        value: NumberNode
+    ): PrintScriptInterpreterState {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValueToVariable(
+        variableNameNode: VariableNameNode,
+        value: StringNode
+    ): PrintScriptInterpreterState {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValueToVariable(
+        variableNameNode: VariableNameNode,
+        value: VariableNameNode
+    ): PrintScriptInterpreterState {
+        TODO("Not yet implemented")
+    }
 }
