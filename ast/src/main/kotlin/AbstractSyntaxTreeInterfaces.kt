36aba419a7e1e25cbad8dc14d359b9c1d430aa16
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-04-13 17:30:43 -0300

    Extracted Sum

--- ast/src/main/kotlin/AbstractSyntaxTreeInterfaces.kt
+++ ast/src/main/kotlin/AbstractSyntaxTreeInterfaces.kt
@@ -73,19 +73,6 @@ enum class Type {
 interface Operable : AbstractSyntaxTree
 interface Operation : Operable
 interface Sum : Operation
-data class NumberNumberSum(val leftOperable: NumberLiteralNode, val rightOperable: NumberLiteralNode) : Sum {
-    fun leftNumber(): Int =
-        leftOperable.number
-    fun rightNumber(): Int =
-        rightOperable.number
-}
-data class NumberStringSum(val leftOperable: NumberLiteralNode, val rightOperable: StringLiteralNode) : Sum
-data class StringNumberSum(val leftOperable: StringLiteralNode, val rightOperable: NumberLiteralNode) : Sum
-data class StringStringSum(val leftOperable: StringLiteralNode, val rightOperable: StringLiteralNode) : Sum
-data class VariableStringSum(val leftOperable: VariableNameNode, val rightOperable: StringLiteralNode) : Sum
-data class StringVariableSum(val leftOperable: StringLiteralNode, val rightOperable: VariableNameNode) : Sum
-data class VariableNumberSum(val leftOperable: VariableNameNode, val rightOperable: NumberLiteralNode) : Sum
-data class NumberVariableSum(val leftOperable: NumberLiteralNode, val rightOperable: VariableNameNode) : Sum
 data class Sub<T : Operable, U : Operable> (val leftOperable: T, val rightOperable: T) : Operation
 data class Mult<T : Operable, U : Operable> (val leftOperable: T, val rightOperable: T) : Operation
 data class Div<T : Operable, U : Operable> (val leftOperable: T, val rightOperable: T) : Operation
