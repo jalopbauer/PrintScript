34845dfc9c30becfc10e60a675d301348e53ba3c
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-04-14 00:23:02 -0300

    StringConcatenation

--- ast/src/main/kotlin/AstOperations.kt
+++ ast/src/main/kotlin/AstOperations.kt
@@ -1,6 +1,6 @@
-data class StringConcatNode(private val value: String) : AbstractSyntaxTree {
-    fun value(): String = value
-}
+// String Concatenation
+data class StringConcatenation(val concatenationParameterValues: ArrayDeque<ConcatenationParameter>) : AbstractSyntaxTree
+interface ConcatenationParameter : AbstractSyntaxTree
 
 data class OperationNode<T, U>(val rightNode: T, val operation: String, val leftNumber: IntNumberLiteralLiteral) : AbstractSyntaxTree {
     fun value(): Int = leftNumber.number // TODO ver como transformar operation en un operador valido
