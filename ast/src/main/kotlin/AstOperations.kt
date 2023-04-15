a3ff2fc0359a0856301c79098be3438d0c2fd8d4
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-04-15 00:47:36 -0300

    Operation

--- ast/src/main/kotlin/AstOperations.kt
+++ ast/src/main/kotlin/AstOperations.kt
@@ -3,44 +3,6 @@ data class StringConcatenation(val concatenationParameterValues: ArrayDeque<Conc
 interface ConcatenationParameter : AbstractSyntaxTree
 
 // Operation
-interface Operation<T : OperationParameter, U : OperationParameter> : AbstractSyntaxTree, OperationParameter {
-    fun leftNode(): T
-    fun rightNode(): U
-}
-data class BothOperation<leftT : OperationParameter, leftU : OperationParameter, rightT : OperationParameter, rightU : OperationParameter, T : Operation<leftT, leftU>, U : Operation<rightT, rightU>> (val leftValue: T, val rightValue: U) : Operation<T, U> {
-    override fun leftNode(): T =
-        leftValue
-    override fun rightNode(): U =
-        rightValue
-}
-data class SolvableOperation<T, U, V : NumberLiteral<T>, W : NumberLiteral<U>>(val leftValue: V, val rightValue: W) : Operation<V, W> {
-    override fun leftNode(): V =
-        leftValue
-
-    override fun rightNode(): W =
-        rightValue
-}
-data class LeftIsVariableOperation<U, W : NumberLiteral<U>>(val leftValue: VariableNameNode, val rightValue: W) : Operation<VariableNameNode, W> {
-    override fun leftNode(): VariableNameNode =
-        leftValue
-
-    override fun rightNode(): W =
-        rightValue
-}
-
-data class RightIsVariableOperation<T, V : NumberLiteral<T>>(val leftValue: V, val rightValue: VariableNameNode) : Operation<V, VariableNameNode> {
-    override fun leftNode(): V =
-        leftValue
-
-    override fun rightNode(): VariableNameNode =
-        rightValue
-}
-
-data class BothVariableOperation(val leftValue: VariableNameNode, val rightValue: VariableNameNode) : Operation<VariableNameNode, VariableNameNode> {
-    override fun leftNode(): VariableNameNode =
-        leftValue
-
-    override fun rightNode(): VariableNameNode =
-        rightValue
-}
-sealed interface OperationParameter
+sealed interface OperationParameter : AbstractSyntaxTree
+data class Operation(val left: OperationParameter, val operation: OperationType, val right: OperationParameter) : OperationParameter
+interface OperationType
