5613ae4745aa95a665b7b88e8aa77d210dac5664
Rodri Marchese <rodrimarchese@gmail.com>
2023-04-18 16:18:23 -0300

    Sentence Splitter

--- lexer/src/main/kotlin/LexerSentence.kt
+++ lexer/src/main/kotlin/LexerSentence.kt
@@ -1,14 +1,14 @@
 package lexer
 
 import Sentence
+import SentenceSplitter
 import token.PrintScript
 import token.Token
-import kotlin.text.split
 
 class LexerSentence : Lexer<Sentence> {
 
     override fun buildTokenList(sentence: Sentence): List<Token> {
-        val splitSentence: List<String> = sentence.getString().split(Regex(" |(?<=[:()])|(?=[:()])")).dropLast(1)
+        val splitSentence: List<String> = SentenceSplitter().split(sentence)
         val result = splitSentence.foldIndexed(listOf<Token>()) { position, tokens, item ->
             val possibleToken = PrintScript().build(item, position, sentence.line)
             tokens + possibleToken
