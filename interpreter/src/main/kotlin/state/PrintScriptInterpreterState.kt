a00214c2fa5193e5f26e30e3534f902d8dff1c74
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-04-17 23:45:42 -0300

    PrintScriptInterpreterStateI

--- interpreter/src/main/kotlin/state/PrintScriptInterpreterState.kt
+++ interpreter/src/main/kotlin/state/PrintScriptInterpreterState.kt
@@ -1,45 +1,24 @@
 package state
+
+import InterpreterResponse
+import Literal
+import VariableInstance
+import VariableNameNode
+
 interface PrintScriptInterpreterState : InterpreterState, PrintlnInterpreterState, VariableInterpreterState
-//
-// data class StatefullPrintScriptInterpreterState(
-//    val printList: List<String> = listOf(),
-//    val variableInterpreterState: VariableInterpreterState
-// ) : PrintScriptInterpreterState {
-//    override fun addError(error: Error): PrintScriptInterpreterState =
-//        this.copy(variableInterpreterState = variableInterpreterState.addError(error))
-//
-//    override fun initializeVariable(variableInstance: VariableInstance): PrintScriptInterpreterState =
-//        this.copy(variableInterpreterState = variableInterpreterState.initializeVariable(variableInstance))
-//
-//    override fun println(value: PrintlnAstParameter): PrintScriptInterpreterState =
-//        TODO("Do")
-//
-//        when (value) {
-//            is NumberLiteral<*> -> this.copy(printList = printList + value.value().toString())
-//            is StringLiteral -> this.copy(printList = printList + value.value)
-//            is VariableNameNode ->
-//                variableStringMap[value.value()]
-//                    ?.let { this.copy(printList = printList + value.value()) }
-//                    ?: variableIntegerMap[value.value()]
-//                        ?.let { this.copy(printList = printList + value.value()) }
-//                    ?: this.addError(VariableIsNotDefined())
-//            is Literal -> this.addError(TypeNotSupportedInPrintError())
-//            else -> this.addError(CannotPrintValueError())
-//        }
-//    override fun setValueToVariable(
-//        key: VariableNameNode,
-//        value: AssignationParameterNode<*>
-//    ): PrintScriptInterpreterState =
-//        when (value) {
-//            is IntNumberLiteral,
-//            is DoubleNumberLiteral,
-//            is StringLiteral -> this.copy(variableInterpreterState = variableInterpreterState.setLiteralToVariable(key, value as Literal))
-//            is VariableNameNode -> this.copy(
-//                variableInterpreterState = variableInterpreterState.setVariableValueToVariable(
-//                    key,
-//                    value
-//                )
-//            )
-//            else -> this.addError(NotAcceptableAssignationValueError())
-//        }
-// }
+
+class PrintScriptInterpreterStateI(private val printlnInterpreterState: PrintlnInterpreterState) : PrintScriptInterpreterState {
+    override fun println(value: String): InterpreterResponse =
+        printlnInterpreterState.println(value)
+
+    override fun initializeVariable(variableInstance: VariableInstance): InterpreterResponse =
+        printlnInterpreterState.initializeVariable(variableInstance)
+
+    override fun setLiteralToVariable(key: VariableNameNode, value: Literal): InterpreterResponse =
+        printlnInterpreterState.setLiteralToVariable(key, value)
+    override fun setVariableValueToVariable(key: VariableNameNode, value: VariableNameNode): InterpreterResponse =
+        printlnInterpreterState.setVariableValueToVariable(key, value)
+
+    override fun get(variableName: VariableNameNode): Literal? =
+        printlnInterpreterState.get(variableName)
+}
