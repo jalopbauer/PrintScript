960e7dc0f378825a37d4ebf758c370defeae96ef
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-04-20 16:46:57 -0300

    FormatterRuleProgress

--- formatter/src/main/kotlin/rule/Rule.kt
+++ formatter/src/main/kotlin/rule/Rule.kt
@@ -1,3 +1,45 @@
 package rule
 
-interface Rule
+import AssignationValidListOfTokens
+import DeclarationValidListOfTokens
+import ValidListOfTokens
+import token.Token
+
+interface Rule<T> {
+    fun apply(listOfTokens: T): String
+}
+interface ValidListOfTokensRule<T : ValidListOfTokens> : Rule<T>
+class AddSpaceBeforeAndAfterDeclaration : ValidListOfTokensRule<DeclarationValidListOfTokens> {
+    override fun apply(listOfTokens: DeclarationValidListOfTokens): String =
+        "let ${listOfTokens.type.tokenName()} : ${listOfTokens.variable.value}"
+}
+
+class AddSpaceBeforeDeclaration : ValidListOfTokensRule<DeclarationValidListOfTokens> {
+    override fun apply(listOfTokens: DeclarationValidListOfTokens): String =
+        "let ${listOfTokens.type.tokenName()} :${listOfTokens.variable.value}"
+}
+
+class AddSpaceAfterDeclaration : ValidListOfTokensRule<DeclarationValidListOfTokens> {
+    override fun apply(listOfTokens: DeclarationValidListOfTokens): String =
+        "let ${listOfTokens.type.tokenName()}: ${listOfTokens.variable.value}"
+}
+
+class AddSpaceBeforeAndAfterAssignation(private val tokenListSpacingRule: TokenListSpacingRule) : ValidListOfTokensRule<AssignationValidListOfTokens> {
+    override fun apply(listOfTokens: AssignationValidListOfTokens): String =
+        "${listOfTokens.variable.value} = ${tokenListSpacingRule.apply(listOfTokens.content)}"
+}
+interface TokenListSpacingRule : Rule<List<Token>>
+class OneSpaceBetweenEveryToken : TokenListSpacingRule {
+    override fun apply(listOfTokens: List<Token>): String =
+        listOfTokens.joinToString(separator = " ")
+}
+
+class PreviousSpacingBetweenEveryToken : TokenListSpacingRule {
+    override fun apply(listOfTokens: List<Token>): String =
+        listOfTokens.drop(1).fold(Pair("", 0)) { (string, lastIndex), token ->
+            val tokenValueInStringForm = token.toString()
+            val newString = string + tokenValueInStringForm.padStart(token.position() - lastIndex, ' ')
+            val newLastIndex = lastIndex + tokenValueInStringForm.length
+            Pair(newString, newLastIndex)
+        }.first
+}
