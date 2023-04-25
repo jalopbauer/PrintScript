ae2df5f7459f78914bd9d62256c0b4e5f04eb58d
matigayito <matigayito.mg@gmail.com>
2023-04-25 14:23:56 -0300

    added parser tests

--- parser/src/test/kotlin/ParserImplTest.kt
+++ parser/src/test/kotlin/ParserImplTest.kt
@@ -7,27 +7,27 @@ class ParserImplTest {
     fun declarationParserWorks() {
         val declaration: List<Token> = listOf()
         val declarationParser = DeclarationParser()
-        val declarationAst =  declarationParser.parse()
+        // val declarationAst =  declarationParser.parse()
     }
 
     @Test
     fun assignationParserWorks() {
         val assignation: List<Token> = listOf()
         val assignationParser = AssignationParser()
-        val assignationAst = assignationParser.parse()
+        // val assignationAst = assignationParser.parse()
     }
 
     @Test
     fun declAssiParserWorks() {
         val declAssi: List<Token> = listOf()
         val declAssiParser = DeclarationAssignationParser()
-        val declAssiAst = declAssiParser.parse()
+        // val declAssiAst = declAssiParser.parse()
     }
 
     @Test
     fun printParserWorks() {
         val print: List<Token> = listOf()
         val printParser = PrintlnParser()
-        val printAst = printParser.parse()
+        // val printAst = printParser.parse()
     }
 }
