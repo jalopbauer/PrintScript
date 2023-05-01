662a5393ad6f4dfa4b0f2a56ca023f6a657b7b4f
matigayito <matigayito.mg@gmail.com>
2023-05-01 15:45:17 -0300

    fixing action errors

--- staticCodeAnalyser/src/test/kotlin/ScaTest.kt
+++ staticCodeAnalyser/src/test/kotlin/ScaTest.kt
@@ -1,8 +1,22 @@
 import org.junit.jupiter.api.Test
+import token.VariableLiteralToken
 
 class ScaTest {
 
     @Test
-    fun ScaCreatesCorrectly() {
+    fun scaCreatesCorrectly() {
+        val tokensCamel = listOf(
+            VariableLiteralToken("test_case", 0, 4)
+        )
+        val scaCamel = RuleStaticCodeAnalyser(CheckVariable(CamelCaseRule()))
+        val camelVariable = scaCamel.format(tokensCamel)
+
+        val tokensSnake = listOf(
+            VariableLiteralToken("testCase", 0, 4)
+        )
+        val scaSnake = RuleStaticCodeAnalyser(CheckVariable(SnakeCaseRule()))
+        val snakeVariable = scaSnake.format(tokensSnake)
+
+        assert(false)
     }
 }
