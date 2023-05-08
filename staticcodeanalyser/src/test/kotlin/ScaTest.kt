482c78b9d004bd4cc87d888fc981e82d5e479411
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-05-07 21:43:44 -0300

    NumberLiteralSca

--- staticcodeanalyser/src/test/kotlin/ScaTest.kt
+++ staticcodeanalyser/src/test/kotlin/ScaTest.kt
@@ -1,25 +1,9 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import token.IntNumberLiteralToken
 import token.VariableLiteralToken
 
 class ScaTest {
-
-//    @Test
-//    fun scaCreatesCorrectly() {
-//        val tokensCamel = listOf(
-//            VariableLiteralToken("test_case", 0, 4)
-//        )
-//        val scaCamel = PrintScriptStaticCodeAnalyserFactory().build("")
-//        val camelVariable = scaCamel.format(tokensCamel)
-//
-//        val tokensSnake = listOf(
-//            VariableLiteralToken("testCase", 0, 4)
-//        )
-//        val scaSnake = PrintScriptStaticCodeAnalyserFactory().build("snake-case-variable")
-//        val snakeVariable = scaSnake.format(tokensSnake)
-//
-//        assert(true)
-//    }
     @Test
     fun onlyCamelCaseSendSnakeCaseShowsError() {
         val variableName = "test_case"
@@ -56,6 +40,15 @@ class ScaTest {
         val variableName = "test_case"
         val tokensCamel = listOf(
             VariableLiteralToken(variableName, 0, 4)
+        )
+        val scaCamel = PrintScriptStaticCodeAnalyserFactory().build("snake-case-variable")
+        assertEquals(null, scaCamel.format(tokensCamel))
+    }
+
+    @Test
+    fun onlyNumberLiteralShowsNothing() {
+        val tokensCamel = listOf(
+            IntNumberLiteralToken(1, 0, 4)
         )
         val scaCamel = PrintScriptStaticCodeAnalyserFactory().build("snake-case-variable")
         assertEquals(null, scaCamel.format(tokensCamel))
