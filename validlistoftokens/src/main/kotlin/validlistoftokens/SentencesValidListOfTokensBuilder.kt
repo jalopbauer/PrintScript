8f91f1ba6471742848e59ce9d92d6c081b9f8664
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-06-24 14:14:41 -0300

    SentencesValidListOfTokensBuilder

--- validlistoftokens/src/main/kotlin/validlistoftokens/SentencesValidListOfTokensBuilder.kt
+++ validlistoftokens/src/main/kotlin/validlistoftokens/SentencesValidListOfTokensBuilder.kt
@@ -1,9 +1,31 @@
 package validlistoftokens
 
+import token.SemicolonToken
 import token.Token
 
-class SentencesValidListOfTokensBuilder : ValidListOfTokensBuilder<SentencesValidListOfTokens> {
-    override fun validate(tokens: List<Token>): SentencesValidListOfTokens? {
-        TODO("Not yet implemented")
-    }
+class SentencesValidListOfTokensBuilder(private val validListOfTokensBuilders: ValidListOfTokensBuilder<SentenceValidListOfTokens>) : ValidListOfTokensBuilder<SentencesValidListOfTokens> {
+    override fun validate(tokens: List<Token>): SentencesValidListOfTokens? =
+        if (tokens.last() is SemicolonToken) { // If semicolon not last is always invalid
+            val sentenceValidListOfTokens =
+                tokens.fold(TokensAndSentenceValidListOfTokens()) { (tokens, validListOfTokens), token -> // Split every SemicolonToken and validate it
+                    if (token is SemicolonToken) {
+                        TokensAndSentenceValidListOfTokens(
+                            listOf(),
+                            validListOfTokens + validListOfTokensBuilders.validate(tokens)
+                        )
+                    } else {
+                        TokensAndSentenceValidListOfTokens(tokens + token, validListOfTokens)
+                    }
+                }.sentenceValidListOfTokens
+            val sentenceValidListOfTokensWithoutNull = sentenceValidListOfTokens.filterNotNull()
+            if (sentenceValidListOfTokens.size == sentenceValidListOfTokensWithoutNull.size) {
+                SentencesValidListOfTokens(sentenceValidListOfTokensWithoutNull)
+            } else {
+                null
+            }
+        } else {
+            null
+        }
 }
+
+data class TokensAndSentenceValidListOfTokens(val tokens: List<Token> = listOf(), val sentenceValidListOfTokens: List<SentenceValidListOfTokens?> = listOf())
