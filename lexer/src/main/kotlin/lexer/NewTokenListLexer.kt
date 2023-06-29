236ec07943ed86f978db2d619419d932438ee373
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-06-29 19:59:22 -0300

    LexerStateLexerResponse

---
+++ lexer/src/main/kotlin/lexer/NewTokenListLexer.kt
@@ -0,0 +1,27 @@
+package lexer
+
+import lexer.lexerState.IntermediateLexerState
+import lexer.lexerState.TokenFoundLexerState
+import lexer.tokenLexer.FirstVersionPrintScriptLexer
+import token.Token
+
+data class NewTokenListLexer(val firstVersionPrintScriptLexer: FirstVersionPrintScriptLexer) : Lexer<LexerInput<*>, LexerStateLexerResponse> {
+    override fun tokenize(input: LexerInput<*>): LexerStateLexerResponse =
+        LexerStateLexer(firstVersionPrintScriptLexer).tokenize(input).let {
+            when (it) {
+                is IntermediateLexerState -> IntermediateLexerStateResponse(it)
+                is TokenFoundLexerState -> {
+                    val tokenize = TokenFoundLexerStateLexer(firstVersionPrintScriptLexer).tokenize(LexerInput(input.nextChar, it))
+
+                    TokenFoundLexerStateResponse(it.token, tokenize)
+                }
+            }
+        }
+}
+
+sealed interface LexerStateLexerResponse
+
+data class TokenFoundLexerStateResponse(val token: Token, val intermediateLexerState: IntermediateLexerState) : LexerStateLexerResponse
+
+data class IntermediateLexerStateResponse(val intermediateLexerState: IntermediateLexerState) :
+    LexerStateLexerResponse
