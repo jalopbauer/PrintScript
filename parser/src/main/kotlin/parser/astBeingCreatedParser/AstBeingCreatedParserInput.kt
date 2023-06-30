aeb9eac782dbf2df8d26d4da481a158db68d3458
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-06-30 14:25:48 -0300

    removed useless

--- parser/src/main/kotlin/parser/astBeingCreatedParser/AstBeingCreatedParserInput.kt
+++
@@ -1,6 +0,0 @@
-package parser.astBeingCreatedParser
-
-import ast.AbstractSyntaxTree
-import token.Token
-
-data class AstBeingCreatedParserInput(val nextToken: Token, val abstractSyntaxTree: AbstractSyntaxTree?)
