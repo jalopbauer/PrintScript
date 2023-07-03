eb9dc1d74f24e69c042f6bbf9deb3711662d6fa7
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-07-03 16:55:32 -0300

    removed useless

--- parser/src/main/kotlin/parser/parserState/astBeingCreated/SentenceBeingCreated.kt
+++
@@ -1,7 +0,0 @@
-package parser.parserState.astBeingCreated
-
-import token.Token
-
-class SentenceBeingCreated(val tokens: List<Token>) : AstBeingCreated {
-    constructor(nextToken: Token) : this(listOf(nextToken))
-}
