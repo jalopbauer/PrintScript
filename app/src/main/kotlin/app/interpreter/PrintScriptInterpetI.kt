10e8e3092f26cf908ddf22b37a08a70023f99fc2
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-07-04 11:39:32 -0300

    parseStates

--- app/src/main/kotlin/app/interpreter/PrintScriptInterpetI.kt
+++ app/src/main/kotlin/app/interpreter/PrintScriptInterpetI.kt
@@ -10,33 +10,32 @@ import lexer.TokenFoundLexerStateResponse
 import parser.PrintScriptParser
 import parser.parserRespose.AstFound
 import parser.parserRespose.SendToken
+import parser.parserState.ParserState
+import token.Token
 
 class PrintScriptInterpetI : PrintScriptInterpret {
     override fun interpret(nextChar: Char, states: PrintScriptInterpretStates): PrintScriptInterpretStates? {
         val (
-            lexerState,
-            parserState,
-            printScriptInterpreterState
+            lexerState
         ) = states
         val input = LexerInput(nextChar, lexerState)
         return when (val stateLexerResponse = NewTokenListLexer().tokenize(input)) {
             is IntermediateLexerStateResponse -> states.copy(lexerState = stateLexerResponse.intermediateLexerState)
-            is TokenFoundLexerStateResponse -> {
-                val addedTokenParserState = parserState.addToken(stateLexerResponse.token)
-                when (val parse = PrintScriptParser().parse(addedTokenParserState)) {
-                    is SendToken -> states.copy(lexerState = stateLexerResponse.intermediateLexerState, parserState = addedTokenParserState)
-                    is AstFound -> {
-                        interpretStates(
-                            parse.abstractSyntaxTree,
-                            states
-                                .copy(lexerState = stateLexerResponse.intermediateLexerState)
-                                .copy(parserState = addedTokenParserState)
-                        )
-                    }
-                }
-            }
+            is TokenFoundLexerStateResponse -> parseStates(stateLexerResponse.token, states.copy(lexerState = stateLexerResponse.intermediateLexerState))
         }
     }
+
+    private fun parseStates(
+        nextToken: Token,
+        states: PrintScriptInterpretStates
+    ): PrintScriptInterpretStates? =
+        states.parserState.addToken(nextToken)
+            .let { addedTokenParserState ->
+                when (val parse = PrintScriptParser().parse(addedTokenParserState)) {
+                    is SendToken -> states.copy(parserState = addedTokenParserState)
+                    is AstFound -> interpretStates(parse.abstractSyntaxTree, states.copy(parserState = ParserState(parse.tokens)))
+                }
+            }
 
     private fun interpretStates(
         abstractSyntaxTree: AbstractSyntaxTree,
