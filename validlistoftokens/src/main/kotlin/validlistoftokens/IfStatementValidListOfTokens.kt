a82600f6c7a1758e7170d751faab4ce73efba42f
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-07-05 17:46:30 -0300

    IfStatementValidListOfTokens

--- validlistoftokens/src/main/kotlin/validlistoftokens/IfStatementValidListOfTokens.kt
+++ validlistoftokens/src/main/kotlin/validlistoftokens/IfStatementValidListOfTokens.kt
@@ -1,33 +1,8 @@
 package validlistoftokens
 
 import token.BooleanLiteralToken
-import token.IfToken
-import token.LeftCurlyBracketsToken
-import token.LeftParenthesisToken
-import token.RightCurlyBracketsToken
-import token.RightParenthesisToken
-import token.Token
 
 data class IfStatementValidListOfTokens(
     val booleanLiteralToken: BooleanLiteralToken,
     val sentencesValidListOfTokens: SentencesValidListOfTokens
 ) : ValidListOfTokens
-
-class IfStatementValidListOfTokensBuilder(private val sentencesValidListOfTokensBuilder: SentencesValidListOfTokensBuilder) : ValidListOfTokensBuilder<IfStatementValidListOfTokens> {
-    override fun validate(tokens: List<Token>): IfStatementValidListOfTokens? {
-        return if (
-            tokens.size >= 6 &&
-            tokens.component1() is IfToken &&
-            tokens.component2() is LeftParenthesisToken &&
-            tokens.component3() is BooleanLiteralToken &&
-            tokens.component4() is RightParenthesisToken &&
-            tokens.component5() is LeftCurlyBracketsToken &&
-            tokens.last() is RightCurlyBracketsToken
-        ) {
-            sentencesValidListOfTokensBuilder.validate(tokens.subList(6, tokens.size - 1))
-                ?.let { IfStatementValidListOfTokens(tokens.component3() as BooleanLiteralToken, it) }
-        } else {
-            null
-        }
-    }
-}
