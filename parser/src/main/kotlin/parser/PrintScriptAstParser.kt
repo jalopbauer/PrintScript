fef5ef7f5ad1cf29553181bee7c65e8d1905be71
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-07-09 15:44:34 -0300

    IfTokenListParser in PrintScriptAstParser

--- parser/src/main/kotlin/parser/PrintScriptAstParser.kt
+++ parser/src/main/kotlin/parser/PrintScriptAstParser.kt
@@ -1,10 +1,11 @@
 package parser
 
 import ast.AbstractSyntaxTree
+import parser.astParsers.ifStatement.IfTokenListParser
 import parser.astParsers.senteceParsers.SentenceParsers
 import token.Token
 
 class PrintScriptAstParser : TokenListParser<AbstractSyntaxTree> {
     override fun parse(tokensInCodeBlock: List<Token>): AbstractSyntaxTree? =
-        SentenceParsers().parse(tokensInCodeBlock)
+        SentenceParsers().parse(tokensInCodeBlock) ?: IfTokenListParser().parse(tokensInCodeBlock)
 }
