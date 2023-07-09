724695c0f63c947ffaade4081bac3fdeb2e2c4a1
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-07-09 17:09:41 -0300

    Made sealed

--- parser/src/main/kotlin/parser/parserState/ParserState.kt
+++ parser/src/main/kotlin/parser/parserState/ParserState.kt
@@ -2,7 +2,7 @@ package parser.parserState
 
 import token.Token
 
-interface ParserState {
+sealed interface ParserState {
     fun tokens(): List<Token>
     fun addToken(token: Token): ParserState
     fun lastAddedToken(): Token?
