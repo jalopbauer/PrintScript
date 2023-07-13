3829c72d173e05272bb106605e014801717fc5a7
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-07-13 15:42:11 -0300

    test

--- app/src/test/kotlin/interpret/P.kt
+++ app/src/test/kotlin/interpret/P.kt
@@ -1,18 +1,31 @@
 package interpret
 
+import app.errorHandler.MyErrorHandler
+import app.interpreter.Interpret
+import app.interpreter.PrintScriptInterpetI
+import app.printer.interpret.PrintScriptInterpretStatesPrinter
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
 
 class P {
 
     @Test
     fun test() {
-//        MyPrintScriptApp().interpret(
-//            ByteArrayInputStream(
+//        val s = """
+//            let pi: number;
+//            pi = 3.14;
+//            println(pi / 2);
 //                """
-//            let numberResult: number = 5 * 5 - 8;
-//            println(numberResult);
-//                """.trimIndent().toByteArray()
-//            )
-//        )
+        val s = """let numberResult: number = 5 * 5 - 8;
+println(numberResult);"""
+
+        Interpret(
+            PrintScriptInterpretStatesPrinter(),
+            PrintScriptInterpetI("", MyErrorHandler())
+        ).interpret(
+            ByteArrayInputStream(
+                s.trimIndent().toByteArray()
+            )
+        )
     }
 }
