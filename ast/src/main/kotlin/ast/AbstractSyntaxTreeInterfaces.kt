3426a2fc527e767c8d9e3c41be8912a3e5fe17b9
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-07-19 01:32:09 -0300

    CorrectedCode

--- ast/src/main/kotlin/ast/AbstractSyntaxTreeInterfaces.kt
+++ ast/src/main/kotlin/ast/AbstractSyntaxTreeInterfaces.kt
@@ -12,10 +12,9 @@ data class VariableNameNode(val variableName: String) :
         variableName
 }
 data class VariableInstance(val variableNameNode: VariableNameNode, val type: Type) : AbstractSyntaxTree
-interface Type : AbstractSyntaxTree
+sealed interface Type : AbstractSyntaxTree
 object StringType : Type
 object NumberType : Type
-object ErrorType : Type
 
 object BooleanType : Type
 class ReadInputAst(val message: StringLiteral) : AbstractSyntaxTree, PrintlnAstParameter, AssignationParameterNode
