8b45762cfe11e0932bf06284bcd09d4f6e85a20d
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-07-19 01:33:44 -0300

    ConstDeclarationAssignationParserTest

--- parser/src/test/kotlin/ConstDeclarationAssignationParserTest.kt
+++ parser/src/test/kotlin/ConstDeclarationAssignationParserTest.kt
@@ -9,7 +9,11 @@ import token.BooleanTypeToken
 import token.ConstToken
 import token.DeclarationToken
 import token.FalseLiteralToken
+import token.LeftParenthesisToken
+import token.ReadInputToken
+import token.RightParenthesisToken
 import token.SemicolonToken
+import token.StringLiteralToken
 import token.VariableNameToken
 class ConstDeclarationAssignationParserTest {
 
@@ -23,6 +27,30 @@ class ConstDeclarationAssignationParserTest {
                 BooleanTypeToken(0, 0),
                 AssignationToken(0, 0),
                 FalseLiteralToken(0, 0),
+                SemicolonToken(0, 0)
+            )
+        )
+        val declarationParser = RegularParserStateParser()
+        val declarationAst = declarationParser.parse(declaration)
+        when (declarationAst) {
+            is AstFound -> assert(declarationAst.abstractSyntaxTree is ConstAssignationDeclarationAst)
+            else -> assert(false)
+        }
+    }
+
+    @Test
+    fun constAssignationDeclarationAstOperation() {
+        val declaration = RegularParserState(
+            listOf(
+                ConstToken(0, 0),
+                VariableNameToken("test", 0, 0),
+                DeclarationToken(0, 0),
+                BooleanTypeToken(0, 0),
+                AssignationToken(0, 0),
+                ReadInputToken(0, 0),
+                LeftParenthesisToken(0, 0),
+                StringLiteralToken("", 0, 0),
+                RightParenthesisToken(0, 0),
                 SemicolonToken(0, 0)
             )
         )
