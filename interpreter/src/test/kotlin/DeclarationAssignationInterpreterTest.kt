5d6c10e3e6231422b472615fe37635ed05c96aed
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-07-18 22:40:39 -0300

    Incorrect types tests

--- interpreter/src/test/kotlin/DeclarationAssignationInterpreterTest.kt
+++ interpreter/src/test/kotlin/DeclarationAssignationInterpreterTest.kt
@@ -2,10 +2,12 @@ import ast.AssignationAst
 import ast.ConstAssignationDeclarationAst
 import ast.DeclarationAst
 import ast.LetAssignationDeclarationAst
+import ast.NumberType
 import ast.ReadInputAst
 import ast.StringLiteral
 import ast.StringType
 import ast.VariableNameNode
+import interpreter.InterpreterError
 import interpreter.PrintScriptInterpreter
 import interpreter.SendLiteral
 import interpreter.state.PrintScriptInterpreterState
@@ -67,6 +69,68 @@ class DeclarationAssignationInterpreterTest {
     }
 
     @Test
+    fun constAlreadySet() {
+        val variableName = "helloVar"
+        val variableNameNode = VariableNameNode(variableName)
+        val type = StringType
+        val declaration = DeclarationAst(variableNameNode, type)
+        val assignationParameter = StringLiteral("hello")
+        val assignation = AssignationAst(variableNameNode, assignationParameter)
+        val constAssignationDeclarationAst = ConstAssignationDeclarationAst(assignation, declaration)
+        val interpreterState = getState(
+            VariableInterpreterStateI(
+                variableTypeMap = mapOf(
+                    variableName to type
+                ),
+                consts = setOf(variableName),
+
+                variableLiteralMap = mapOf(
+                    variableName to assignationParameter
+                )
+            )
+        ).setReadInput(StringLiteral("hello"))
+        val interpreter = PrintScriptInterpreter()
+
+        val interpreterResponse = interpreter.interpret(
+            constAssignationDeclarationAst,
+            interpreterState
+        )
+
+        assert(interpreterResponse is InterpreterError)
+    }
+
+    @Test
+    fun setDifferentTypesOfPreviousVar() {
+        val variableName = "helloVar"
+        val variableNameNode = VariableNameNode(variableName)
+        val type = StringType
+        val declaration = DeclarationAst(VariableNameNode("newVar"), NumberType)
+        val assignationParameter = StringLiteral("hello")
+        val assignation = AssignationAst(VariableNameNode("newVar"), variableNameNode)
+        val constAssignationDeclarationAst = ConstAssignationDeclarationAst(assignation, declaration)
+        val interpreterState = getState(
+            VariableInterpreterStateI(
+                variableTypeMap = mapOf(
+                    variableName to type
+                ),
+                consts = setOf(variableName),
+
+                variableLiteralMap = mapOf(
+                    variableName to assignationParameter
+                )
+            )
+        ).setReadInput(StringLiteral("hello"))
+        val interpreter = PrintScriptInterpreter()
+
+        val interpreterResponse = interpreter.interpret(
+            constAssignationDeclarationAst,
+            interpreterState
+        )
+
+        assert(interpreterResponse is InterpreterError)
+    }
+
+    @Test
     fun letAssignationDeclaration() {
         val variableNameNode = VariableNameNode("helloVar")
         val type = StringType
@@ -85,5 +149,26 @@ class DeclarationAssignationInterpreterTest {
         )
 
         assert(interpreterResponse is PrintScriptInterpreterState)
+    }
+
+    @Test
+    fun letAssignationDeclarationWrongType() {
+        val variableNameNode = VariableNameNode("helloVar")
+        val type = NumberType
+        val declaration = DeclarationAst(variableNameNode, type)
+        val assignationParameter = ReadInputAst(StringLiteral("hello"))
+        val assignation = AssignationAst(variableNameNode, assignationParameter)
+        val letAssignationDeclarationAst = LetAssignationDeclarationAst(assignation, declaration)
+        val interpreterState = getState(
+            VariableInterpreterStateI(variableTypeMap = mapOf())
+        ).setReadInput(StringLiteral("hello"))
+        val interpreter = PrintScriptInterpreter()
+
+        val interpreterResponse = interpreter.interpret(
+            letAssignationDeclarationAst,
+            interpreterState
+        )
+
+        assert(interpreterResponse is InterpreterError)
     }
 }
